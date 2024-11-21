7b3c3b60c7ed4e54ed595c4c020e069b19ef1ee9
Ido Hatam <ihatam@BIOL-67714-L.local>
2024-11-20 17:51:04 -0800

    Split the conda_check function into multiple functions that would be called by a wrapper function.

--- conda_check.R
+++ conda_check.R
@@ -1,31 +1,9 @@
-#this function will check if miniconda is already installed on the computer and will update to most recent version
-
-
+#this function checks if miniconda is installed on the computer.
 conda_check <- function(){
   #Check if conda is installed
-  chk <- Sys.which("conda")
-  if(nchar(chk)==0){
-    stop(paste("Miniconda isn't installed or path isn't set properly, 
-               either set the path or run the conda_dl function"))
+  if(nchar(Sys.which("conda"))==0){
+    return(FALSE)
   } else{
-    #Check that channels are ok
-    channels <- system("conda config --show channels", 
-                       intern = TRUE)
-    
-    if(length(which(grepl(paste(c("conda-forge", "bioconda"), 
-                                collapse = "|"), channels))) ==2){
-      stop(paste("We're good to go, proceed with package instalation via the package_setup function"))
-      
-    } else {
-      system("conda config --add channels bioconda")
-      system("conda config --add channels conda-forge")
-      system("conda config --set channel_priority strict")
-      print("Conda channels are now set, 
-            proceed with package instalation via the package_setup function")
-    }
-    }
-  
-  
-  
-                         }
+    return(TRUE)}
+  }
   
