9eb41d68f9dab9fbdb337781ea4c5a1467fc34bf
Ido Hatam <ihatam@BIOL-67714-L.local>
2025-01-21 12:28:21 -0800

    Finished the wrapper function and incorportated the check_step helper function

--- R/whats_installed.R
+++ R/whats_installed.R
@@ -9,8 +9,9 @@ conda_whats_installed <- function(){
   if(!conda_check()){
     message("Conda isn't installed, miniconda will be downloaded and installed")
     report$conda_dl_inst <- conda_dl()
-    if(any(grepl("fail",report$conda_dl_inst,ignore.case = FALSE))){
-      message("Setup warkflow stopped: 
+    check_step(report$conda_dl_inst, "")
+    if(any(grepl("fail",report$conda_dl_inst, ignore.case = TRUE))){
+      message("Setup workflow stopped: 
               Miniconda installation encountered the following issue, 
               please resulove it and try again")
       print(report$conda_dl_inst)
@@ -30,7 +31,7 @@ conda_whats_installed <- function(){
   report$conda_channel_setup <- conda_channels_setup()
   #s
   if(any(grepl("fail", report$conda_channel_setup, ignore.case = TRUE))){
-    message("Setup warkflow stopped because at least one chennel setup failed")
+    message("Setup workflow stopped because at least one channel setup failed")
     print(report$conda_channel_setup)
     stop()
   }else{
@@ -43,7 +44,7 @@ conda_whats_installed <- function(){
   report$conda_update <- conda_update()
   
   if(any(grepl("fail", report$conda_update, ignore.case = TRUE))){
-    message("Setup warkflow stopped because at least one chennel setup failed")
+    message("Setup workflow stopped because at least one channel setup failed")
     print(report$conda_update_setup)
     stop()
   }else{
@@ -57,10 +58,10 @@ conda_whats_installed <- function(){
   report$conda_tool_setup <- conda_check_tools()
   
   if(any(grepl("fail", report$conda_tool_setup, ignore.case = TRUE))){
-    warning("Instalation of at least one of the 3rd party tools failed \n installation of R packages will continue however make sure to check the issue")
-    print(report$conda_update_setup)
+    warning("installation of at least one of the 3rd party tools failed installation of R packages will continue however make sure to check the issue")
+    print(report$conda_tool_setup)
   }else{
-    message("The required 3 party tools were successfully installed")
+    message("The required 3rd party tools were successfully installed")
   }
   
   #install R packages
@@ -69,8 +70,8 @@ conda_whats_installed <- function(){
   report$R_packages_setup <- package_setup()
   
   if(any(grepl("fail", report$R_packages_setup, ignore.case = TRUE))){
-    warning("Instalation of at least one one package failed \n installation of R packages will continue however make sure to check the issue")
-    print(report$conda_update_setup)
+    warning("installation of at least one one package failed installation of R packages will continue however make sure to check the issue")
+    print(report$R_package_setup)
   }else{
     message("The required R packages were successfully installed")
   }
