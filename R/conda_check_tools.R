3958aea91835a1c357b277658b41d175eb046fb1
Ido Hatam <ihatam@BIOL-67714-L.local>
2025-04-23 13:37:47 -0700

    Added an installation of mamba

--- R/conda_check_tools.R
+++ R/conda_check_tools.R
@@ -25,31 +25,28 @@
 
 conda_check_tools <- function(){
   
-  tool_list <- c("histat2", "samtools", "bowtie2", "wget", "bwa")
-  installed_tools <- system2("conda", args = "list",
-                             stdout = TRUE, stderr = FALSE)
+  tool_list <- c("hisat2", "samtools", "bowtie2", "wget", "bwa", "mamba")
+  installed_tools <- system2("conda", args = "list", stdout = TRUE, stderr = FALSE)
   tools_status <- list()
   
   for (tool in tool_list) {
-    #check tools
-    if(any(grepl(tool, installed_tools))){
-      tools_status <- append(tools_status, 
-                             paste0(tool, "' is already installed."))
-    }else{
-    #install wget with conda
-    tryCatch({if(tool=="wget"){
-      system2("conda", args = c("install", "-y", tool, 
-                                stdout = FALSE, stderr = FALSE))
-    }else{
-      #install everything else with mamba
-      system2("mamba", args = c("install", "-y", tool, 
-                                stdout = FALSE, stderr = FALSE))
+    # Check if tool is installed
+    if (any(grepl(tool, installed_tools))) {
+      tools_status <- append(tools_status, paste0(tool, " is already installed."))
+    } else {
+      # Install wget and mamba with conda
+      tryCatch({
+        if (tool == "wget" | tool == "mamba") {
+          system2("conda", args = c("install", "-y", tool))
+        } else {
+          # Install other tools with mamba
+          system2("mamba", args = c("install", "-y", tool))
+        }
+        tools_status <- append(tools_status, paste0(tool, " was installed successfully."))
+      }, error = function(e) {
+        tools_status <- append(tools_status, paste(tool, "installation failed with error:", e$message))
+      })
     }
-    tools_status <- append(tools_status, paste0(tool, "' was installed succesfully."))},
-    error = function(e){
-      tools_status <- append(tools_status, paste(tool, "installation failed with error:", e$message))
-    })
-      }
   }
   
   return(unlist(tools_status))
